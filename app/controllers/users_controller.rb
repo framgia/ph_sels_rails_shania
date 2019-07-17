f5b01d5b24d8a4f663213cd0f68b396f373e74d7
shania98 <shania.jubelag@sun-asterisk.com>
2019-07-17 18:28:31 +0800

    User Index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+before_action :logged_in_user, only: [:index]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -12,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to PokeLearning!"
+      flash[:success] = "Welcome to the PokeLearning!"
       redirect_to @user
     else
       render 'new'
@@ -24,5 +29,12 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
