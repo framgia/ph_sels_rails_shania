36a8b2ea8ef30a4b3fe63a7d67cc865488a3b5e4
shania98 <shania.jubelag@sun-asterisk.com>
2019-07-17 21:19:23 +0800

    Fixed indentions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-before_action :logged_in_user, only: [:index]
+  before_action :logged_in_user, only: [:index]
 
   def index
     @users = User.paginate(page: params[:page])
