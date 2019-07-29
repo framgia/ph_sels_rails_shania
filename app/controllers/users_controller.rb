cf8a4b9ca5e1a5bf7cccf6e8ea7026bcd669b5e3
shania98 <shania.jubelag@sun-asterisk.com>
2019-07-29 21:28:29 +0800

    Following Function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,12 +47,24 @@ class UsersController < ApplicationController
       redirect_to users_url
     end
 
-      
     def check_signed_in
       redirect_to(root_url) if logged_in?
     end
+
+    def following
+      @title = "Following"
+      @user  = User.find(params[:id])
+      @users = @user.following.paginate(page: params[:page])
+      render 'show_follow'
+    end
+  
+    def followers
+      @title = "Followers"
+      @user  = User.find(params[:id])
+      @users = @user.followers.paginate(page: params[:page])
+      render 'show_follow'
+    end
       
-    
     private
     
     def user_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,32 @@
-User.create!(name:  "Example User",
+User.create!(name:  "Admin User",
   email: "example@railstutorial.org",
   password:              "foobar",
   password_confirmation: "foobar",
   admin: true)
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
+#User
+45.times do |n|
+  name  = Faker::TvShows::DrWho.character
+  email = "example-#{n+1}@sun-asterisk.com"
+  password = "sun-asterisk"
   User.create!(name:  name,
       email: email,
       password:              password,
       password_confirmation: password)
 end
+
+#Category
+12.times do
+  title = Faker::Games::Pokemon.name
+  description = Faker::Games::Pokemon.move
+  Category.create!(title:  title,
+    description: description)
+end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
